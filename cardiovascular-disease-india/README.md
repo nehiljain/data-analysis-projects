f1c7a1f048db26b0ff6ddc063390339541d2e214
nehiljain <jain.nehil@gmail.com>
2014-04-16 20:14:06 -0300

    data muning going on

--- cardiovascular-disease-india/README.md
+++ cardiovascular-disease-india/README.md
@@ -4,4 +4,6 @@ EDA and Visualisation on Cardiovascular Disease in India Dataset.
 Web reference: http://visualising.org/datasets/cardiovascular-disease-india
 Dataset Source : http://visualising.org/sites/default/files/data_set/edward_lee/india_chd_data.xlsx
 
+The First Challenge I face is that the data is not formatted with any standard. So I have to do some data munging and remove many redundant rows and variables.
+
 
--- cardiovascular-disease-india/munge/01-A.R
+++ cardiovascular-disease-india/munge/01-A.R
@@ -1,3 +1,14 @@
-# Example preprocessing script.
+#Preprocessing script.
 
+#setting the names right
+names(india.chd.data.DALYs) <- normVarNames(names(india.chd.data.DALYs))
+names(india.chd.data.Deaths) <- normVarNames(names(india.chd.data.Deaths))
+names(india.chd.data.Prevalence) <- normVarNames(names(india.chd.data.Prevalence))
+names(india.chd.data.Years.Lost) <- normVarNames(names(india.chd.data.Years.Lost))
 
+#DALYs table cleaning.. This is a fragile HARD CODED part.
+india.chd.data.DALYs$na <- NULL
+india.chd.data.DALYs <- india.chd.data.DALYs[-1,]
+india.chd.data.DALYs <- india.chd.data.DALYs[1:27,]
+dalys_population <- india.chd.data.DALYs[1,5]
+india.chd.data.DALYs <- india.chd.data.DALYs[-1,]
