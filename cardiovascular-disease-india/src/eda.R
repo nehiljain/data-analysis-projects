4d7ff9e926f737054743577887a146bcddb9c7fb
nehiljain <jain.nehil@gmail.com>
2014-04-23 15:55:41 -0300

    exploration of daeths distribution in males and females using historgrams

--- cardiovascular-disease-india/src/eda.R
+++ cardiovascular-disease-india/src/eda.R
@@ -1,9 +1,20 @@
+
 library('ProjectTemplate')
 load.project()
 
-for (dataset in project.info$data)
-{
-  message(paste('Showing top 5 rows of', dataset))
-  print(head(get(dataset)))  
-}
+#EDA in Deaths
 
+print(summary(india.chd.data.Deaths$value))
+print(summary(log10(india.chd.data.Deaths$value)))
+
+qplot(x = value, data=filter(india.chd.data.Deaths, gbd_sub_case != "NA", gender != "person"), binwidth=10) +
+  facet_wrap(~gender) +
+  scale_x_continuous(breaks = seq(1,500,20), limits=c(0, 500)) + 
+  scale_y_continuous(breaks = seq(1,40,2))
+qplot(x = value, data=filter(india.chd.data.Deaths, gbd_sub_case != "NA", gender != "person"), binwidth=20) +
+  facet_wrap(~gender) +
+  scale_x_continuous(limits=c(500, 1000))
+
+ggplot(aes(x=value), data=filter(india.chd.data.Deaths, gbd_sub_case != "NA", gender != "person")) +
+  geom_freqpoly(aes(color=gender)) +
+  scale_x_continuous(breaks = seq(1,500,20), limits=c(0, 500)) 
